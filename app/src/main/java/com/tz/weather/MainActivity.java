d34cbaca7e9dc56f65b3618ad2a8c1ef2ea3ea1b
TimothyZhukov <TimaZhukov096666096>
2022-03-20 21:34:52 +0200

    Code optimization and fixes

--- app/src/main/java/com/tz/weather/MainActivity.java
+++ app/src/main/java/com/tz/weather/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -20,73 +19,33 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String targetUrl;
     private final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&appid=9ad44b0c121776b58879fb126eea524a";
-    private EditText editTextCityOrZip;
+    private EditText editTextCityOrIndex;
     private TextView textViewCityWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextCityOrZip = findViewById(R.id.editTextCityOrZip);
+        editTextCityOrIndex = findViewById(R.id.editTextCityOrIndex);
         textViewCityWeather = findViewById(R.id.textViewCityWeather);
     }
 
-    public void onClickGo(View view) {
-        String editTextString = editTextCityOrZip.getText().toString().trim();
-        if (!editTextString.isEmpty()) {
-            if (isNumeric(editTextString)) {
-                targetUrl = "https://api.openweathermap.org/data/2.5/weather?id=" + editTextString + "&units=metric&appid=9ad44b0c121776b58879fb126eea524a";
-            } else {
-                targetUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + editTextString + "&units=metric&appid=9ad44b0c121776b58879fb126eea524a";
-            }
+    public void onClickShowWeather(View view) {
+        String city = editTextCityOrIndex.getText().toString().trim();
+        if (!city.isEmpty()) {
+            DownloadWeatherTask task = new DownloadWeatherTask();
+            String url = String.format(WEATHER_URL, city);
+            task.execute(url);
         } else {
             Toast.makeText(this, getString(R.string.toastAttention), Toast.LENGTH_SHORT).show();
         }
-
-        DownloadJSONTask task = new DownloadJSONTask();
-        String result = null;
-        try {
-            result = task.execute(targetUrl).get();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            JSONObject jsonObject = new JSONObject(result);
-            String name = jsonObject.getString("name");
-//                Log.i("Result", name);
-            JSONObject main = jsonObject.getJSONObject("main");
-            String temp = main.getString("temp");
-//                Log.i("Result", "Temperature: " + temp);
-            JSONArray jsonArray = jsonObject.getJSONArray("weather");
-            JSONObject weather = jsonArray.getJSONObject(0);
-            String mainStr = weather.getString("main");
-            String descriptionStr = weather.getString("description");
-//                Log.i("Result", "Main: " + mainStr);
-            textViewCityWeather.setText(name + "\nTemperature: " + temp + "\n" + mainStr);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 
-    public static boolean isNumeric(String str) {
-        try {
-            Double.parseDouble(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    private static class DownloadJSONTask extends AsyncTask<String, Void, String> {
+    private class DownloadWeatherTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... strings) {
@@ -117,24 +76,19 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
 
-//        @Override
-//        protected void onPostExecute(String s) {
-//            super.onPostExecute(s);
-//            try {
-//                JSONObject jsonObject = new JSONObject(s);
-//                String name = jsonObject.getString("name");
-////                Log.i("Result", name);
-//                JSONObject main = jsonObject.getJSONObject("main");
-//                String temp = main.getString("temp");
-////                Log.i("Result", "Temperature: " + temp);
-//                JSONArray jsonArray = jsonObject.getJSONArray("weather");
-//                JSONObject weather = jsonArray.getJSONObject(0);
-//                String mainStr = weather.getString("main");
-//                String descriptionStr = weather.getString("description");
-////                Log.i("Result", "Main: " + mainStr);
-//            } catch (JSONException e) {
-//                e.printStackTrace();
-//            }
-//        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String city = jsonObject.getString("name");
+                String temp = jsonObject.getJSONObject("main").getString("temp");
+                String description = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
+                String weather = String.format("%s\nTemperature: %s\nOutdoors: %s", city, temp, description);
+                textViewCityWeather.setText(weather);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
